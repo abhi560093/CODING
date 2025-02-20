ab90041229b616f708cc00ae1483ff983ec83274
ABHINAV <abhinav.lucknow98@gmail.com>
2025-02-20 11:00:44 +0530

    Add files via upload

---
+++ Calculator for Addition.py
@@ -0,0 +1,17 @@
+print("Hello Coders")
+print("This is a calculator for you.")
+print ("This is Calculator for Addition")
+first = input("Enter first number : ")
+Second = input("Enter Second number : ")
+print("ANSWER")
+sum = int(first) + int(Second)
+print(sum)
+
+print("Enter the Rewiew")
+first = input("Enter your rewiew : ")
+
+quotient = first * first
+print(quotient)
+
+
+
